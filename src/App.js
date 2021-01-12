69c60b28cce877f10aa349ee16dbad8af9226edf
Ryan-Larke <ryanlarke@gmail.com>
2021-01-12 22:59:52 +1100

    display 10 days of month

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 const apiUrl = 'http://localhost:3001'
 
+
 class App extends Component {
 
   state = {}
@@ -20,7 +21,14 @@ class App extends Component {
     console.log(files);
   }
 
+
   render() {
+    
+    const days = 10;
+    const datesToDisplay = [];
+    for(let i = 0; i< days; i++){
+      datesToDisplay.push(new Date(i+1 + "Mar 2018"))
+    }
     return (
       <div className="App">
         <div className="App-header">
@@ -46,6 +54,36 @@ class App extends Component {
               )
             })
           }
+          <p>Existing bookings timeline:</p>
+          <div style={{display: "flex", flexDirection: "column"}}>
+          {
+            datesToDisplay.map((day) => {
+              return <div style={{ width: "1000px", height: "40px", margin: "5px", padding: "10px", backgroundColor: "red" }}>
+                {day.toLocaleDateString()}
+              </div>
+            })
+
+          }  
+          </div>
+
+          <div style={{display: "flex"}}>
+          {
+            (this.state.bookings || []).map((booking, i) => {
+              const date = new Date(booking.time);
+              const duration = booking.duration / (60 * 1000);
+              return (
+                <div></div>
+
+                // <p key={i} className="App-booking">
+                //   <span className="App-booking-time">{date.toString()}</span>
+                //   <span className="App-booking-duration">{duration.toFixed(1)}</span>
+                //   <span className="App-booking-user">{booking.userId}</span>
+                // </p>
+              )
+            })
+          }
+          </div>
+         
         </div>
       </div>
     );
